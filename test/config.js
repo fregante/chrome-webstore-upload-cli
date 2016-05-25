0bf5879441593c48c54c3277b67d56ae8e5a3289
Andrew Levine <theandrewtizer@gmail.com>
2016-05-24 20:14:10 -0500

    Add test to verify options work as env vars

--- test/config.js
+++ test/config.js
@@ -10,6 +10,26 @@ test('Favors params over env vars', t => {
     delete process.env.EXTENSION_ID;
 });
 
+test('All options supported as env vars', t => {
+    const vars = [
+        'EXTENSION_ID',
+        'CLIENT_ID',
+        'CLIENT_SECRET',
+        'REFRESH_TOKEN'
+    ];
+    const varsVal = "123";
+
+    vars.forEach(name => process.env[name] = varsVal);
+
+    const config = createConfig(null, {});
+    t.is(config.apiConfig.extensionId, varsVal);
+    t.is(config.apiConfig.clientId, varsVal);
+    t.is(config.apiConfig.clientSecret, varsVal);
+    t.is(config.apiConfig.refreshToken, varsVal);
+
+    vars.forEach(name => delete process.env[name]);
+});
+
 test('Extracts zip path', t => {
     const file = 'foo.zip';
     const config = createConfig(null, { file });
